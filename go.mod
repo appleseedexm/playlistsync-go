7b073784750916967183a283cb2a68cc2912ac6b
appleseedexm <appleseedexm@gmail.com>
2024-04-02 00:30:24 +0200

    read playlists from spotify

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module playlistsync/main
 
 go 1.22.1
+
+require (
+	github.com/bitly/go-simplejson v0.5.1 // indirect
+	github.com/moul/http2curl v1.0.0 // indirect
+	github.com/parnurzeal/gorequest v0.3.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rapito/go-spotify v0.0.0-20201203225102-8089a3ff3172 // indirect
+	golang.org/x/net v0.22.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/rapito/go-spotify/spotify"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	fmt.Println("Hello, World!")
+	fmt.Println("Hello, World!")
+
+	spot := spotify.New(nil,nil)
+	spot.Authorize()
+
+	result, _ := spot.Get("playlists/%s", nil, "5QIZGd9DfVRrcDJJPBISQv")
+
+	var playlistResponse PlaylistResponse
+
+	error := json.Unmarshal([]byte(result), &playlistResponse)
+	fmt.Println(error)
+
+	fmt.Println(string((playlistResponse.AllTracks.TrackWithMetaData[0].Track.Artists[0].Name)))
+
+}
+
+type PlaylistResponse struct {
+	AllTracks AllTracks `json:"tracks"`
+}
+
+type AllTracks struct {
+	TrackWithMetaData []TrackMetaData `json:"items"`
+}
+
+type TrackMetaData struct {
+	Track Track `json:"track"`
+}
+
+type Track struct {
+	Name    string   `json:"name"`
+	Artists []Artist `json:"artists"`
+}
+
+type Artist struct {
+	Name string `json:"name"`
 }
